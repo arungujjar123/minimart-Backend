8c906b6c38ced1b61d0db63dd4b51f2397884243
arungujjar123 <officialarunbagujjar@gmail.com>
2025-08-21 20:49:17 +0530

    backend env update

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ process.on("SIGINT", async () => {
   );
   process.exit(0);
 });
-
+const PORT = process.env.PORT || 5000;
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
@@ -56,7 +56,7 @@ mongoose
   .then(() => {
     console.log("✅ MongoDB Atlas connected successfully");
     console.log("Database:", mongoose.connection.name);
-    app.listen(5000, () => console.log("🚀 Server running on port 5000"));
+    app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
   })
   .catch((err) => {
     console.error("❌ MongoDB Atlas connection error:", err.message);
